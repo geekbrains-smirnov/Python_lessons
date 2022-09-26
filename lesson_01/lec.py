95fac0d69964e7fa7bbbd4e33ef886717e26ccc3
Artem Smirnov <expertvideo.as@gmail.com>
2022-09-26 13:13:14 +0300

    add lesson_01

---
+++ lesson_01/lec.py
@@ -0,0 +1,24 @@
+#print("hello world")
+#a = 123
+#b = 1.23
+#print(type(a))
+#print(type(b))
+#s = 'hello world'
+#print(a,'-', b, '-', s)
+#print('{} - {} - {}'.format(a, b, s))
+#print(f'{a} - {b} - {s}')
+
+#f = True
+#print(f)
+
+#list = [1,3,4]
+#print(list)
+
+#print('Введите a: ')
+#a = int(input())
+#print('Введите b: ')
+#b = int(input())
+#print(a,'-',b, '=', a+b)
+#print(f'{a} - {b}')
+#print('{} - {}'.format(a,b))
+
